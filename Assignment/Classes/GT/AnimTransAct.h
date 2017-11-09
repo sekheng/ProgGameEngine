1bb9cebe4f2830ccb550d63e76da084977b8ad08
Ah Heng <leesekheng@yahoo.com>
2017-11-09 22:01:17 +0800

    Changed the namespace to be consistent with the filename

--- Assignment/Classes/GT/AnimTransAct.h
+++ Assignment/Classes/GT/AnimTransAct.h
@@ -6,26 +6,28 @@
 /** To provide a simper transitioning between the actions so that it will be able to make use of other ActionInterval such as DelayTime
 * Should only be used by AnimationHandlerNode!
 */
-class AnimTransAct : public cocos2d::ActionInterval
+namespace GinTama
 {
-public:
-    static AnimTransAct* create();
-    static AnimTransAct* create(const std::string &_AnimStateName);
+    class AnimTransAct : public cocos2d::ActionInterval
+    {
+    public:
+        static AnimTransAct* create();
+        static AnimTransAct* create(const std::string &_AnimStateName);
 
-    AnimTransAct* setAnimStateName(const std::string &_AnimStateName);
+        AnimTransAct* setAnimStateName(const std::string &_AnimStateName);
 
-    virtual void startWithTarget(cocos2d::Node *target);
-    /** Wait till i find a purpose with this
-    */
-    virtual void update(float time);
-    virtual void stop();
+        virtual void startWithTarget(cocos2d::Node *target);
+        /** Wait till i find a purpose with this
+        */
+        virtual void update(float time);
+        virtual void stop();
 
-CC_CONSTRUCTOR_ACCESS:
-    virtual ~AnimTransAct();
-protected:
-    AnimTransAct();
+    CC_CONSTRUCTOR_ACCESS:
+        virtual ~AnimTransAct();
+    protected:
+        AnimTransAct();
 
-    AnimationHandlerNode *m_targetNode;
-    std::string m_transitStateName;
-};
-
+        AnimationHandlerNode *m_targetNode;
+        std::string m_transitStateName;
+    };
+}
