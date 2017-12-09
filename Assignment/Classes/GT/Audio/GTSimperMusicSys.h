88d99c2ec843ab64c06c794e137971f6f74b7dda
Terry Lim <lnxterry@gmail.com>
2017-12-09 21:20:16 +0800

    Rename textures file from Obstacles to Obstacle.

--- Assignment/Classes/GT/Audio/GTSimperMusicSys.h
+++ Assignment/Classes/GT/Audio/GTSimperMusicSys.h
@@ -1,44 +1,54 @@
 #pragma once
 
+// Include Cocos
 #include "cocos2d.h"
-#include "MK/Common/MKSingletonTemplate.h"
+
+// Include Others
 #include "external/json/document.h"
 
-namespace GinTama {
-    struct GTSoundData
-    {
-        float m_Volume;
-        int m_AudioID = -1;
-        std::string m_fileLocation;
-        std::string m_SoundName;
-        bool m_Loop;
-    };
+// Include MK
+#include "MK/Common/MKSingletonTemplate.h"
 
-    class GTSimperMusicSys : public MinamiKotori::MKSingletonTemplate<GTSimperMusicSys>
-    {
-    public:
-        bool playSound(const std::string &_songName);
+// Include GT
+#include "GT/Common/GTMacros.h"
 
-        GTSoundData* accessSound(const std::string &_songName);
+NS_GT_BEGIN
 
-        bool stopSound(const std::string &_songName);
-        bool pauseSound(const std::string &_songName);
-        bool resumeSound(const std::string &_songName);
-
-        bool setSoundVol(const std::string &_songName, const float &_vol);
-        bool setSoundVol(GTSoundData* _song, const float &_vol);
-        bool setSoundLoop(const std::string &_songName, const bool &_loop);
-
-        void setMasterVol(const float &_vol);
-        float getMasterVol();
-
-        GTSimperMusicSys();
-    protected:
-        virtual ~GTSimperMusicSys();
-
-        float m_MasterVol;
-        RAPIDJSON_NAMESPACE::Document m_MusicDocs;
-        std::unordered_map<std::string, GTSoundData*> m_NameSoundMap;
-        std::map<std::string, std::list<GTSoundData*> > m_NamePlayingSound;
-    };
+struct GTSoundData
+{
+    float m_Volume;
+    int m_AudioID = -1;
+    std::string m_fileLocation;
+    std::string m_SoundName;
+    bool m_Loop;
 };
+
+class GTSimperMusicSys : public MinamiKotori::MKSingletonTemplate<GTSimperMusicSys>
+{
+public:
+    bool playSound(const std::string &_songName);
+
+    GTSoundData* accessSound(const std::string &_songName);
+
+    bool stopSound(const std::string &_songName);
+    bool pauseSound(const std::string &_songName);
+    bool resumeSound(const std::string &_songName);
+
+    bool setSoundVol(const std::string &_songName, const float &_vol);
+    bool setSoundVol(GTSoundData* _song, const float &_vol);
+    bool setSoundLoop(const std::string &_songName, const bool &_loop);
+
+    void setMasterVol(const float &_vol);
+    float getMasterVol();
+
+    GTSimperMusicSys();
+protected:
+    virtual ~GTSimperMusicSys();
+
+    float m_MasterVol;
+    RAPIDJSON_NAMESPACE::Document m_MusicDocs;
+    std::unordered_map<std::string, GTSoundData*> m_NameSoundMap;
+    std::map<std::string, std::list<GTSoundData*> > m_NamePlayingSound;
+};
+
+NS_GT_END
